4cb3c2f2b8eb69b55488f49a0bae0097323fab7c
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:09 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libmdport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libmdport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libmdport.git'), string(name: 'PORT_DESCRIPTION', value: 'This library provides message digest functions found on BSD systems either' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libmdport.git'), string(name: 'PORT_DESCRIPTION', value: 'This library provides message digest functions found on BSD systems either' )]
   }
 }
